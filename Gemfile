fd6867a232880ec06b9114b521764c0f14711566
BMR731 <1547963731@qq.com>
2016-07-16 08:30:41 +0800

    complete the user microposts

--- Gemfile
+++ Gemfile
@@ -11,6 +11,12 @@ gem 'faker'
 gem 'will_paginate'
 gem 'bootstrap-will_paginate'
 
+# to upload a image
+gem 'carrierwave'
+gem 'mini_magick'
+gem 'fog'
+
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0'
 # Use sqlite3 as the database for Active Record
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destory]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :delete
   def new
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
-  	
+  	@microposts = @user.microposts.paginate(page: params[:page])
   end
 
 
@@ -56,13 +56,7 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
 
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please login"
-        redirect_to login_url
-      end
-    end
+
 
     def correct_user
         @user = User.find(params[:id])
